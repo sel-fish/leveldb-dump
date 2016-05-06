64e97b3d3db0af30a699d935d6ac622a10fa4863
sel-fish <fqlgy@hotmail.com>
2016-05-06 14:07:10 +0800

    implement dump

--- leveldb_dump.cpp
+++ leveldb_dump.cpp
@@ -1,5 +1,21 @@
+#include <iostream>
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+#include <leveldb/db.h>
+#include <leveldb/options.h>
+#include <leveldb/comparator.h>
+#include <leveldb/env.h>
+
+bool g_stop = false;
+
+void print_help(const char* name);
+void sign_handler(int sig);
+int do_key_dump(const char* db_path, const char* manifest, const char* dump_file);
+leveldb::Status open_db_readonly(const char* db_path, const char* manifest,
+                                 leveldb::Options& options, leveldb::DB*& db);
+
+using namespace std;
 
 int main(int argc, char** argv)
 {
@@ -22,8 +38,117 @@ int main(int argc, char** argv)
       dump_file = optarg;
       break;
     default:
-      // print_help(argv[0]);
+      print_help(argv[0]);
       return 1;
     }
   }
+
+  if (db_path == NULL || manifest_file == NULL || dump_file == NULL)
+  {
+    print_help(argv[0]);
+    return 1;
+  }
+
+  signal(SIGINT, sign_handler);
+  signal(SIGTERM, sign_handler);
+
+	return do_key_dump(db_path, manifest_file, dump_file);
+}
+
+void print_help(const char* name)
+{
+  fprintf(stderr, "dump ldb data to file\n"
+          "%s -p db_path -f manifestfile -d dump_file",
+          name);
+}
+
+void sign_handler(int sig)
+{
+  switch (sig)
+  {
+  case SIGTERM:
+  case SIGINT:
+    fprintf(stderr, "catch sig %d\n", sig);
+    g_stop = true;
+    break;
+  default:
+    break;
+  }
+}
+
+int do_key_dump(const char* db_path, const char* manifest, const char* dump_file)
+{
+  int dump_fd = -1;
+
+  // open db
+  leveldb::Options open_options;
+  leveldb::DB* db = NULL;
+  leveldb::Status s = open_db_readonly(db_path, manifest, open_options, db);
+  if (!s.ok())
+  {
+    fprintf(stderr, "open db fail: %s\n", s.ToString().c_str());
+    return 1;
+  }
+
+  // get db iterator
+  leveldb::ReadOptions scan_options;
+  scan_options.verify_checksums = false;
+  scan_options.fill_cache = false;
+  leveldb::Iterator* db_it = db->NewIterator(scan_options);
+
+  for (db_it->SeekToFirst(); !g_stop && db_it->Valid(); db_it->Next())
+  {
+    string key(const_cast<char*>(db_it->key().data()) + 2, db_it->key().size() - 2);
+    cout << key << endl;
+  }
+
+  if (dump_fd > 0)
+  {
+    close(dump_fd);
+  }
+
+  if (db_it != NULL)
+  {
+    delete db_it;
+  }
+  if (db != NULL)
+  {
+    delete db;
+    delete open_options.comparator;
+    delete open_options.env;
+    delete open_options.info_log;
+  }
+
+	return 0;
+
+}
+
+leveldb::Status open_db_readonly(const char* db_path, const char* manifest,
+                                 leveldb::Options& options, leveldb::DB*& db)
+{
+  options.error_if_exists = false; // exist is ok
+  options.create_if_missing = false;
+  // options.table_cache_size = 20 << 20;
+  // options.env = leveldb::Env::Instance();
+
+  char buf[32];
+  snprintf(buf, sizeof(buf), "./tmp_ldb_log.%d", getpid());
+  leveldb::Status s = options.env->NewLogger(buf, &options.info_log);
+  if (s.ok())
+  {
+    s = leveldb::DB::Open(options, db_path, manifest, &db);
+  }
+
+  if (!s.ok())
+  {
+    fprintf(stderr, "open db with mainfest fail: %s", s.ToString().c_str());
+    delete options.comparator;
+    delete options.env;
+    if (options.info_log != NULL)
+    {
+      delete options.info_log;
+    }
+  }
+
+  return s;
 }
